ef37063f2d9bf8f8c7ea3548225bf35bad0a7bd6
liuyangxing <x@loui.cc>
2026-03-09 20:33:45 +0800

    feat(ui): add Card and Selector components

--- src/pages/ui/card/index.tsx
+++ src/pages/ui/card/index.tsx
@@ -12,6 +12,8 @@ import {
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
+import { Switch } from "@/components/ui/switch";
+import { BellRing, Check } from "lucide-react-taro";
 import {
   Select,
   SelectContent,
@@ -20,43 +22,134 @@ import {
   SelectValue,
 } from "@/components/ui/select";
 
+const notifications = [
+  {
+    title: "Your call has been confirmed.",
+    description: "1 hour ago",
+  },
+  {
+    title: "You have a new message!",
+    description: "1 hour ago",
+  },
+  {
+    title: "Your subscription is expiring soon!",
+    description: "2 hours ago",
+  },
+];
+
 export default function CardPage() {
   return (
     <PageLayout title="Card">
-      <Card className="w-full">
-        <CardHeader>
-          <CardTitle>Create project</CardTitle>
-          <CardDescription>
-            Deploy your new project in one-click.
-          </CardDescription>
-        </CardHeader>
-        <CardContent>
-          <View className="grid w-full items-center gap-4">
-            <View className="flex flex-col space-y-2">
-              <Label>Name</Label>
-              <Input id="name" placeholder="Name of your project" />
+      <View className="grid gap-6">
+        <Card className="w-full">
+          <CardHeader>
+            <CardTitle>Create project</CardTitle>
+            <CardDescription>
+              Deploy your new project in one-click.
+            </CardDescription>
+          </CardHeader>
+          <CardContent>
+            <View className="grid w-full items-center gap-4">
+              <View className="flex flex-col gap-2">
+                <Label>Name</Label>
+                <Input id="name" placeholder="Name of your project" />
+              </View>
+              <View className="flex flex-col gap-2">
+                <Label for="framework">Framework</Label>
+                <Select
+                  defaultValue="next"
+                  value={undefined}
+                  onValueChange={undefined}
+                  open={undefined}
+                  defaultOpen={undefined}
+                  onOpenChange={undefined}
+                >
+                  <SelectTrigger id="framework">
+                    <SelectValue placeholder="Select" />
+                  </SelectTrigger>
+                  <SelectContent>
+                    <SelectItem value="next">Next.js</SelectItem>
+                    <SelectItem value="sveltekit">SvelteKit</SelectItem>
+                    <SelectItem value="astro">Astro</SelectItem>
+                    <SelectItem value="nuxt">Nuxt.js</SelectItem>
+                  </SelectContent>
+                </Select>
+              </View>
             </View>
-            <View className="flex flex-col space-y-2">
-              <Label>Framework</Label>
-              <Select>
-                <SelectTrigger id="framework">
-                  <SelectValue placeholder="Select" />
-                </SelectTrigger>
-                <SelectContent>
-                  <SelectItem value="next">Next.js</SelectItem>
-                  <SelectItem value="sveltekit">SvelteKit</SelectItem>
-                  <SelectItem value="astro">Astro</SelectItem>
-                  <SelectItem value="nuxt">Nuxt.js</SelectItem>
-                </SelectContent>
-              </Select>
+          </CardContent>
+          <CardFooter className="flex justify-between">
+            <Button variant="outline">Cancel</Button>
+            <Button>Deploy</Button>
+          </CardFooter>
+        </Card>
+
+        <Card className="w-full">
+          <CardHeader>
+            <CardTitle>Notifications</CardTitle>
+            <CardDescription>You have 3 unread messages.</CardDescription>
+          </CardHeader>
+          <CardContent className="grid gap-4">
+            <View className="flex items-center rounded-md border p-4">
+              <BellRing size={24} className="mr-4" />
+              <View className="flex-1 space-y-1">
+                <View className="text-sm font-medium leading-none">
+                  Push Notifications
+                </View>
+                <View className="text-sm text-muted-foreground">
+                  Send notifications to device.
+                </View>
+              </View>
+              <Switch />
             </View>
-          </View>
-        </CardContent>
-        <CardFooter className="flex justify-between">
-          <Button variant="outline">Cancel</Button>
-          <Button>Deploy</Button>
-        </CardFooter>
-      </Card>
+            <View>
+              {notifications.map((notification, index) => (
+                <View
+                  key={index}
+                  className="mb-4 grid last:mb-0 last:pb-0"
+                  style={{ gridTemplateColumns: "25px 1fr" }}
+                >
+                  <View className="flex h-2 w-2 translate-y-1 rounded-full bg-sky-500" />
+                  <View className="space-y-1">
+                    <View className="text-sm font-medium leading-none">
+                      {notification.title}
+                    </View>
+                    <View className="text-sm text-muted-foreground">
+                      {notification.description}
+                    </View>
+                  </View>
+                </View>
+              ))}
+            </View>
+          </CardContent>
+          <CardFooter>
+            <Button className="w-full">
+              <Check className="mr-2 h-4 w-4" size={16} /> Mark all as read
+            </Button>
+          </CardFooter>
+        </Card>
+
+        <Card className="w-full">
+          <CardHeader>
+            <CardTitle className="text-2xl">Login</CardTitle>
+            <CardDescription>
+              Enter your email below to login to your account.
+            </CardDescription>
+          </CardHeader>
+          <CardContent className="grid gap-4">
+            <View className="grid gap-2">
+              <Label for="email">Email</Label>
+              <Input id="email" placeholder="m@example.com" />
+            </View>
+            <View className="grid gap-2">
+              <Label for="password">Password</Label>
+              <Input id="password" password />
+            </View>
+          </CardContent>
+          <CardFooter>
+            <Button className="w-full">Sign in</Button>
+          </CardFooter>
+        </Card>
+      </View>
     </PageLayout>
   );
 }
--- src/pages/ui/pagination/index.tsx
+++ src/pages/ui/pagination/index.tsx
@@ -1,4 +1,5 @@
 
+import { View } from "@tarojs/components";
 import { PageLayout } from "@/components/page-layout";
 import {
   Pagination,
@@ -13,30 +14,69 @@ import {
 export default function PaginationPage() {
   return (
     <PageLayout title="Pagination">
-      <Pagination>
-        <PaginationContent>
-          <PaginationItem>
-            <PaginationPrevious href="#" />
-          </PaginationItem>
-          <PaginationItem>
-            <PaginationLink href="#">1</PaginationLink>
-          </PaginationItem>
-          <PaginationItem>
-            <PaginationLink href="#" isActive>
-              2
-            </PaginationLink>
-          </PaginationItem>
-          <PaginationItem>
-            <PaginationLink href="#">3</PaginationLink>
-          </PaginationItem>
-          <PaginationItem>
-            <PaginationEllipsis />
-          </PaginationItem>
-          <PaginationItem>
-            <PaginationNext href="#" />
-          </PaginationItem>
-        </PaginationContent>
-      </Pagination>
+      <View className="grid gap-10">
+        <View className="grid gap-2">
+          <View className="text-sm font-medium leading-none">Default</View>
+          <Pagination>
+            <PaginationContent>
+              <PaginationItem>
+                <PaginationPrevious />
+              </PaginationItem>
+              <PaginationItem>
+                <PaginationLink>1</PaginationLink>
+              </PaginationItem>
+              <PaginationItem>
+                <PaginationLink isActive>
+                  2
+                </PaginationLink>
+              </PaginationItem>
+              <PaginationItem>
+                <PaginationLink>3</PaginationLink>
+              </PaginationItem>
+              <PaginationItem>
+                <PaginationEllipsis />
+              </PaginationItem>
+              <PaginationItem>
+                <PaginationNext />
+              </PaginationItem>
+            </PaginationContent>
+          </Pagination>
+        </View>
+
+        <View className="grid gap-2">
+          <View className="text-sm font-medium leading-none">Simple</View>
+          <Pagination>
+            <PaginationContent>
+              <PaginationItem>
+                <PaginationPrevious />
+              </PaginationItem>
+              <PaginationItem>
+                <PaginationNext />
+              </PaginationItem>
+            </PaginationContent>
+          </Pagination>
+        </View>
+
+        <View className="grid gap-2">
+          <View className="text-sm font-medium leading-none">Numbers Only</View>
+          <Pagination>
+            <PaginationContent>
+              <PaginationItem>
+                <PaginationLink>1</PaginationLink>
+              </PaginationItem>
+              <PaginationItem>
+                <PaginationLink isActive>2</PaginationLink>
+              </PaginationItem>
+              <PaginationItem>
+                <PaginationLink>3</PaginationLink>
+              </PaginationItem>
+              <PaginationItem>
+                <PaginationLink>4</PaginationLink>
+              </PaginationItem>
+            </PaginationContent>
+          </Pagination>
+        </View>
+      </View>
     </PageLayout>
   );
 }
